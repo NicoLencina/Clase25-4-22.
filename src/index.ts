6c0316b91ab2235076cde8637473ce7cbff2f2f5
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-02 20:01:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("datos");
+let rotulo = document.getElementById("rotulo");
+rotulo.innerHTML = "ingrese un dato:";
+btnEnv.addEventListener("click", () => {
+  // tu codigo va aqui, por ejemplo:
+  console.log("el dato ingresado es", dato.value);
+  //
+});
