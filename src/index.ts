bff30737d169939278349b40172fcfda58bc5aa5
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-02 23:59:22 -0300

    Corregido

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+//declaro las variables y accedo  a los valores de HTML con la funcion document.getElementById.
+
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("datos");
 let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "ingrese un dato:";
-btnEnv.addEventListener("click", () => {
-  // tu codigo va aqui, por ejemplo:
-  console.log("el dato ingresado es", dato.value);
+
+//con .innerHTML nos permite leer el dato de rotulo o asignarle uno nuevo
+
+rotulo.innerHTML = "Ingrese un dato:";
+btnEnv.addEventListener("click", () => 
+
+// Imprimo en consola el valor de la variable dato, .value evalua exactamente lo de adentro de dato.
+  console.log("el dato ingresado es", dato.value;
   //
 });
